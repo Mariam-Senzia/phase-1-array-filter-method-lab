76fe8dc6902a659d09897247996b19ab28161512
Mariam Senzia <mariamsenzia@gmail.com>
2023-12-20 08:21:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, attribute) {
+    
+     return drivers.filter(function (driver) { 
+        return  driver.toLowerCase() === attribute.toLowerCase()
+    });
+}
+console.log(findMatching());
+
+function fuzzyMatch (drivers,attribute) {
+    //const drivers = []
+   // drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() 
+    })
+
+}
+
+function matchName(drivers,attribute) {
+    return drivers.filter(function (driver) { return driver.name === attribute})
+}
